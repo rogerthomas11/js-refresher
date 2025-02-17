105c26ce4f82f58bbf18562dd5093acf72c95af3
Roger Thomas <rogerthomas96@outlook.com>
2025-02-16 20:52:06 -0600

    completed refesher sections on operators, arrow functions, parameters, objects, and classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,49 @@
 // import { apiKeyExample } from "./utility.js";
 
-import apiKeyExample from "./utility.js";
+// import apiKeyExample from "./utility.js";
 
-console.log(apiKeyExample)
+// console.log(apiKeyExample)
+
+// const userMessage = "Hello world!!";
+
+// console.log(userMessage);
+
+// console.log("operators" + "are" + "fun!");
+
+// function greeting() {
+//     console.log("Welcome!!!!");
+// }
+
+// greeting();
+
+// export default (userName, message) => {
+//     console.log("Hello");
+//     return userName + message;
+// }
+
+// const user = {
+//     name: "Roger",
+//     age: 28,
+//     greet() {
+//         console.log("Hey there!");
+//         console.log(this.name);
+//     }
+// };
+
+// console.log(user.name);
+// user.greet();
+
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {
+//         console.log("Hey there!");
+//     }
+// }
+
+// const newUser = new User("John", 33);
+// console.log(newUser);
+// newUser.greet();
--- assets/scripts/utility.js
+++ assets/scripts/utility.js
@@ -1,3 +1,3 @@
 // export let apiKeyExample = "al2nmvcreiomnsfvx";
  
-export default "al2nmvcreiomnsfvx";
+// export default "al2nmvcreiomnsfvx";
