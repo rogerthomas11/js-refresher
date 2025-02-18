a43ff7fb50d941862b306c8d4955bea75c354d55
Roger Thomas <rogerthomas96@outlook.com>
2025-02-17 21:56:37 -0600

    completed arrays, array functions, destructuring, and added labels for code review sections

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,22 @@
+//IMPORTS & EXPORTS
+
 // import { apiKeyExample } from "./utility.js";
 
 // import apiKeyExample from "./utility.js";
 
 // console.log(apiKeyExample)
 
+//VARIABLES & VALUES
+
 // const userMessage = "Hello world!!";
 
 // console.log(userMessage);
 
+//OPERATORS
+
 // console.log("operators" + "are" + "fun!");
+
+//FUNCTIONS & PARAMETERS
 
 // function greeting() {
 //     console.log("Welcome!!!!");
@@ -16,10 +24,14 @@
 
 // greeting();
 
+//ARROW FUNCTIONS
+
 // export default (userName, message) => {
 //     console.log("Hello");
 //     return userName + message;
 // }
+
+//OBJECTS & CLASSES
 
 // const user = {
 //     name: "Roger",
@@ -47,3 +59,49 @@
 // const newUser = new User("John", 33);
 // console.log(newUser);
 // newUser.greet();
+
+//ARRAYS & ARRAY METHODS
+
+// const hobbies = ["gardening", "hockey", "woodworking"];
+
+// console.log(hobbies[2]);
+
+// hobbies.push("coding");
+
+// const index = hobbies.findIndex((item) => item === "hockey");
+
+// console.log(index);
+
+// const editedHobbies = hobbies.map((item) => item + "!");
+
+// console.log(editedHobbies);
+
+// const objectHobbies = hobbies.map((item) => ({text: item}));
+
+// console.log(objectHobbies);
+
+//DESTRUCTURING
+
+// const [firstName, lastName] = ["Roger", "Thomas"];
+
+// // const firstName = userNameData[0];
+// // const lastName = userNameData[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const user = {
+// name: "Roger",
+// age: 28
+// };
+
+// const name = user.name;
+// const age = user.age;
+
+// const {name, age} = {
+//     name: "Roger",
+//     age: 28
+// }
+
+// console.log(name);
+// console.log(age);
